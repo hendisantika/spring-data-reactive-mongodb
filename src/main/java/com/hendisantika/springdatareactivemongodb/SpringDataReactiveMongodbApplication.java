9e3c5504762059f4b49cb9f7565a4948b9a34d60
Hendi Santika <hendisantika@gmail.com>
2019-01-13 21:20:12 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springdatareactivemongodb/SpringDataReactiveMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springdatareactivemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataReactiveMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataReactiveMongodbApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/hendisantika/springdatareactivemongodb/SpringDataReactiveMongodbApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springdatareactivemongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataReactiveMongodbApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
