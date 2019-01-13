74813f77c306e4e9dacc28679cfbd158eb2f2419
Hendi Santika <hendisantika@gmail.com>
2019-01-13 21:32:58 +0700

    #6 - Modify Main App

--- src/main/java/com/hendisantika/springdatareactivemongodb/SpringDataReactiveMongodbApplication.java
+++ src/main/java/com/hendisantika/springdatareactivemongodb/SpringDataReactiveMongodbApplication.java
@@ -1,14 +1,46 @@
 package com.hendisantika.springdatareactivemongodb;
 
+import com.hendisantika.springdatareactivemongodb.document.Person;
+import com.hendisantika.springdatareactivemongodb.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
-public class SpringDataReactiveMongodbApplication {
+public class SpringDataReactiveMongodbApplication implements CommandLineRunner {
+
+    @Autowired
+    private PersonRepository personRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataReactiveMongodbApplication.class, args);
     }
 
+    @Override
+    public void run(String[] args) {
+
+        final Person johnAoe = new Person("john", "aoe", LocalDateTime.now(), "loser", 0);
+        final Person johnBoe = new Person("john", "boe", LocalDateTime.now(), "a bit of a loser", 10);
+        final Person johnCoe = new Person("john", "coe", LocalDateTime.now(), "average", 100);
+        final Person johnDoe = new Person("john", "doe", LocalDateTime.now(), "winner", 1000);
+
+        personRepository.saveAll(Flux.just(johnAoe, johnBoe, johnCoe, johnDoe)).subscribe();
+
+        personRepository
+                .findByFirstName("john")
+                .log()
+                .map(Person::getSecondName)
+                .subscribe(System.out::println);
+
+        personRepository
+                .findOneByFirstName("john")
+                .log()
+                .map(Person::getId)
+                .subscribe(System.out::println);
+    }
 }
 
