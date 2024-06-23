0db37e825eac8242289146202ac03e69ad37c4de
hendisantika <hendisantika@yahoo.co.id>
2024-06-23 08:45:25 +0700

    Add Log Level Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springdatareactivemongodb/document/LogLevel.java
@@ -0,0 +1,15 @@
+package com.hendisantika.springdatareactivemongodb.document;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-reactive-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/23/24
+ * Time: 08:45
+ * To change this template use File | Settings | File Templates.
+ */
+public enum LogLevel {
+    ERROR, WARN, INFO
+}
