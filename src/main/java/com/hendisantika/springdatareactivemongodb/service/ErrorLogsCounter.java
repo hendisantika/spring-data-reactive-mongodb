1a82431c4716a9ded57afc3e6e2825135efd507e
hendisantika <hendisantika@yahoo.co.id>
2024-06-23 08:46:18 +0700

    Add ErrorLogsCounter🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springdatareactivemongodb/service/ErrorLogsCounter.java
@@ -0,0 +1,28 @@
+package com.hendisantika.springdatareactivemongodb.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.messaging.MessageListenerContainer;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-reactive-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/23/24
+ * Time: 08:46
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+public class ErrorLogsCounter implements LogsCounter {
+
+    private static final String LEVEL_FIELD_NAME = "level";
+
+    private final String collectionName;
+    private final MessageListenerContainer container;
+
+    private final AtomicInteger counter = new AtomicInteger();
+
+}
