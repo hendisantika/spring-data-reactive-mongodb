f9698b2145740c9f31e17f63949be024bcb1a643
hendisantika <hendisantika@yahoo.co.id>
2024-06-23 09:05:32 +0700

    Add MongoDBTestContainerConfig🫸🌀✏️📗🐧🐳

---
+++ src/test/java/com/hendisantika/springdatareactivemongodb/MongoDBTestContainerConfig.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springdatareactivemongodb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-data-reactive-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/23/24
+ * Time: 09:02
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableMongoRepositories
+public class MongoDBTestContainerConfig {
+    @Container
+    public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0-rc-jammy")
+            .withExposedPorts(27017);
+
+    static {
+        mongoDBContainer.start();
+        var mappedPort = mongoDBContainer.getMappedPort(27017);
+        System.setProperty("mongodb.container.port", String.valueOf(mappedPort));
+    }
+}
